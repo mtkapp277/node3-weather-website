8dc5c43d9cc6914e63708b1082a1fb116ab2f01c
Kapp <Michael.CTR.Kapp@faa.gov>
2019-12-16 22:14:47 -0500

    Initial Commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoibXRrYXBwMjc3IiwiYSI6ImNrMmNiamJqNTF1ZW8zY2xzZThyOXBuYW8ifQ.FsMCRxL0cc2ttpPWHYGBfg&limit=1'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
